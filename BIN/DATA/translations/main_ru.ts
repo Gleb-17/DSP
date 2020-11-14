69ad018052df930849920cc7f2ffc10329fcec7d
Gleb Mikhaylov <migleb@yandex,ru>
2020-11-14 23:13:05 +0300

    fix translation

--- BIN/DATA/translations/main_ru.ts
+++ BIN/DATA/translations/main_ru.ts
@@ -4,23 +4,44 @@
 <context>
     <name>Widget</name>
     <message>
-        <location filename="../../../DSP_audio/widget.cpp" line="78"/>
+        <location filename="../../../DSP_audio/widget.cpp" line="46"/>
         <source>Choose source path</source>
         <translation>Выберите исходный каталог</translation>
     </message>
     <message>
-        <location filename="../../../DSP_audio/widget.cpp" line="83"/>
-        <location filename="../../../DSP_audio/widget.cpp" line="94"/>
+        <location filename="../../../DSP_audio/widget.cpp" line="50"/>
+        <location filename="../../../DSP_audio/widget.cpp" line="60"/>
         <source>Choose</source>
         <translation>Выбрать</translation>
     </message>
     <message>
-        <location filename="../../../DSP_audio/widget.cpp" line="89"/>
+        <location filename="../../../DSP_audio/widget.cpp" line="56"/>
         <source>Choose destination path</source>
         <translation>Выберите каталог назначения</translation>
     </message>
     <message>
-        <location filename="../../../DSP_audio/widget.cpp" line="121"/>
+        <location filename="../../../DSP_audio/widget.cpp" line="66"/>
+        <source>Parametres of algorytm</source>
+        <translation>Параметры алгоритма</translation>
+    </message>
+    <message>
+        <location filename="../../../DSP_audio/widget.cpp" line="22"/>
+        <source>Start algorytm</source>
+        <translation>Старт</translation>
+    </message>
+    <message>
+        <location filename="../../../DSP_audio/widget.cpp" line="76"/>
+        <source>Threshold level (silence), dB</source>
+        <translation>Пороговый уровень тишины, дБ</translation>
+    </message>
+    <message>
+        <location filename="../../../DSP_audio/widget.cpp" line="120"/>
+        <source>Algorytm is started</source>
+        <translation>Алгоритм запущен</translation>
+    </message>
+    <message>
+        <location filename="../../../DSP_audio/widget.cpp" line="134"/>
+        <location filename="../../../DSP_audio/widget.cpp" line="147"/>
         <source>Choose directory</source>
         <translation>Выберите каталог</translation>
     </message>
